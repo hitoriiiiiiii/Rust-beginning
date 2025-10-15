9ea93a8c2569dc17645a511a68aa1b58fe725b43
GeniusPrarthana <Prarthanagade25@gmail.com>
2025-10-15 20:06:09 +0530

    fibbo

--- src/fibonnaci.rs
+++ src/fibonnaci.rs
@@ -1,21 +1,20 @@
-fn main() {
-    println!("{}", fib(10));
+fn main(){
+    printIn!("{}", fib(10));
 }
 
-fn fib(num: u32) -> u32 {
+fn fib(num: u32){
     let mut first = 0;
     let mut second = 1;
 
-    if num == 0 {
+    if (num == 0){
         return first;
-    } else if num == 1 {
+    } else if (num == 1){
         return second;
     }
-
-    for _ in 0..(num - 1) {
+    for _ in 0 ..(num-2){
         let temp = second;
         second = first + second;
         first = temp;
     }
-    second
+    return second;
 }
